080713c377ad03b7c6c383f77fe11316364f927c
salem asfaw <salemasfaw7@gmail.com>
2026-07-17 19:56:54 +0300

    add all

--- backend/config/email.js
+++ backend/config/email.js
@@ -3,9 +3,8 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true only for port 465
-
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
