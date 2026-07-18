d57f73e5b11dd6186ad4c69424335dba12d7f21a
salem asfaw <salemasfaw7@gmail.com>
2026-07-18 17:01:06 +0300

    add all

--- backend/config/email.js
+++ backend/config/email.js
@@ -2,21 +2,34 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
+
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+
+  port: 587,
+
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
 });
 
+
 transporter.verify((error, success) => {
+
   if (error) {
-    console.error("EMAIL ERROR:", error);
+
+    console.log("EMAIL ERROR:", error);
+
   } else {
+
     console.log("EMAIL SERVER READY");
+
   }
+
 });
+
 
 module.exports = transporter;
