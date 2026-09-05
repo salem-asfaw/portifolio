8bd36888cb5e2395585282c7f9de380e4dfc3f52
salem asfaw <salemasfaw7@gmail.com>
2026-09-05 15:57:09 +0300

    Add Evangadi certificate to portfolio

--- frontend/src/components/Certificates/Certificates.jsx
+++ frontend/src/components/Certificates/Certificates.jsx
@@ -5,36 +5,39 @@ export default function Certificates() {
     {
       title: "Full Stack Development",
       place: "Evangadi Tech",
-      year: "2026"
+      year: "2026",
+      certificate: "../../../public/img/certificate.png",
     },
   ];
+
   return (
     <section id="certificates" className={styles.certificates}>
       <div className={styles.container}>
-        <h2>
-          Certificates
-        </h2>
+        <h2>Certificates</h2>
+
         <div className={styles.timeline}>
-          {
-            certificates.map((item,index)=>(
-              <div 
-                className={styles.card}
-                key={index}
-              >
-                <div className={styles.year}>
-                  {item.year}
-                </div>
-                <div>
-                  <h3>
-                    {item.title}
-                  </h3>
-                  <p>
-                    {item.place}
-                  </p>
-                </div>
+          {certificates.map((item, index) => (
+            <div className={styles.card} key={index}>
+              <div className={styles.year}>
+                {item.year}
               </div>
-            ))
-          }
+
+              <div className={styles.content}>
+                <h3>{item.title}</h3>
+
+                <p>{item.place}</p>
+
+                <a
+                  href={item.certificate}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.button}
+                >
+                  View Certificate
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
